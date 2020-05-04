48741143e231ba027a7ca3604bdb59a750b6e8c5
SimonOkello <simonokello93@gmail.com>
2020-05-04 17:30:05 +0300

    Added UI

--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,15 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
-from .views import user_list, message_list
+from .views import user_list, message_list, index,chat_view, register, message_view
 
 urlpatterns = [
+    path('', index, name = 'index' ),
+    path('register', register, name = 'register'),
+    path('chat/', chat_view, name = 'chats' ),
+    path('chat/<int:sender>/<int:receiver>', message_view, name='chat'),
     path('api/messages/<int:sender>/<int:receiver>/', message_list, name='message-detail'),
     path('api/messages/', message_list, name= 'message-list'),
     path('api/users/', user_list, name='users'),
     path('api/users/<int:pk>/', user_list, name='user-detail'),
+    path('logout', LogoutView.as_view(next_page = 'index'), name='logout'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import User
-from django.http.response import JsonResponse
+from django.http.response import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .models import Message
 from .serializers import MessageSerializer, UserSerializer
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
 # User view
@@ -25,7 +28,7 @@ def user_list(request, pk=None):
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
 
-# Message View
+# Message list View
 @csrf_exempt
 def message_list(request, sender=None, receiver=None):
     if request.method=='GET':
@@ -40,3 +43,44 @@ def message_list(request, sender=None, receiver=None):
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
 
+# index view
+def index(request):
+    if request.user.is_authenticated:
+        return redirect('chats')
+    if request.method=='GET':
+        return render(request, 'index.html', {})
+    if request.method == 'POST':
+        username, password = request.POST['username'], request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+        else:
+            return HttpResponse('{"error": "User does not exist"}')
+        return redirect('chats')
+
+# chats view
+def chat_view(request):
+    if not request.user.is_authenticated:
+        return redirect('index')
+    if request.method == 'GET':
+        return render(request, 'chat.html', {'users': User.objects.exclude(username=request.user.username)})
+    
+
+# register view
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    return render(request, 'register.html', {})
+
+# message view
+
+def message_view(request, sender, receiver):
+    if not request.user.is_authenticated:
+        return redirect('index')
+    if request.method == 'GET':
+        return render(request, 'message.html', {
+            'users': User.objects.exclude(username = request.user.username),
+            'receiver': User.objects.get(id=receiver),
+            'messages': Message.objects.filter(sender_id=sender, receiver_id =receiver) |
+                        Message.objects.filter(sender_id=receiver, receiver_id=sender)
+        })
